eeeacccbae658cf94f463c5503a93b395e6c4b9c
gnomelv <nlainervos@gmail.com>
2026-02-02 12:39:13 -0800

    Update Selectable_Auton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autons/Selectable_Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autons/Selectable_Auton.java
@@ -10,6 +10,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+/*
+Never made this work but it could be really useful for control award next year. And shouldn't be too hard to implement
+The idea is to make the autonomous work sort of like tuning where theres one opmode and you select different versions with it
+An advanced version could be used to make it customizable. like before the match starts the driver can select where the robot
+will go and what it will do by stringing together paths and actions
+IDK how hard it will be to implement though.
+ */
 
 @Disabled
 @Autonomous(name="Selectable Auton")
